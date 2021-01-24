10b0eea045b2a67b11d2d1300856f2dae89dbcf8
Severa3 <reizmp4@gmail.com>
2021-01-24 16:37:08 +0200

    Initial commit version

---
+++ ProPets_hotels/src/main/java/telran/propets/hotels/ProPetsHotelsApplication.java
@@ -0,0 +1,13 @@
+package telran.propets.hotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPetsHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPetsHotelsApplication.class, args);
+	}
+
+}
---
+++ ProPets_hotels/src/test/java/telran/propets/hotels/ProPetsHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.propets.hotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPetsHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
